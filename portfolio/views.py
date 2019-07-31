726da2d1bc1111c716e37cdb0655a1700628f032
seojaehoon <12wogns5@naver.com>
2019-07-31 20:36:01 +0900

    first commit of wordcount project

---
+++ portfolio/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from .models import Portfolio
+
+
+def portfolio(request):
+    portfolios = Portfolio.objects
+    return render(request, 'portfolio.html', {'portfolios':portfolios})
+
+
